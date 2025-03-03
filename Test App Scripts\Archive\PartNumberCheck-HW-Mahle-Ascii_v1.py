0143fcb00b4c8cf7eacc3a0509290a37b0424ed1
hjing-tpi <hjing@tpiusa.com>
2025-03-03 15:17:20 -0500

    Added Test App Scripts\Archive\PartNumberCheck-HW-Mahle-Ascii_v1.py - 2025-03-03T20:10:12.676Z

---
+++ Test App Scripts\Archive\PartNumberCheck-HW-Mahle-Ascii_v1.py
@@ -0,0 +1,54 @@
+﻿#Part Number Check - Hardware   Parse raw data ASCII and don't perform the compare against the broadcasted part number
+#
+#TPass Objects Passed In/Returned
+#   in - string "broadcastedPartNumber"
+#   in - string "rawCanDataResponse"
+#   in - Function "TestAppLogger"
+#   out - string "processedPartNumber
+#   out - string "rawCanDataPartNumberHex"
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+
+#System.Diagnostics.Debugger.Break();
+
+#rawCanDataResponse = "100E62F191414944422D3035392D4141000000000000"
+#broadcastedPartNumber = 'AIDB-059-AA'
+
+import clr
+version = "1.0"
+production = False
+
+# string offset starts with 0
+partNumberHexByteOffset = 10
+partNumberHexLength = 22
+
+isSuccess = True
+try:
+    TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data Response = {0}", rawCanDataResponse)
+    TestAppLogger.Info("Part Number Check Python Script:  Broadcasted Part Number = {0}", broadcastedPartNumber)
+
+    rawCanDataPartNumberHex = rawCanDataResponse[partNumberHexByteOffset:partNumberHexByteOffset+partNumberHexLength]
+    TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data Part Number = {0}", rawCanDataPartNumberHex)
+
+    processedPartNumber = bytes.fromhex(rawCanDataPartNumberHex)
+    TestAppLogger.Info("Part Number Check Python Script:  Processed Part Number = {0}", processedPartNumber)
+#    if broadcastedPartNumber != processedPartNumber:
+#        isSuccess = False
+except Exception as inst:
+    TestAppLogger.Warn("Part Number Check Python Script:  Exception Occurred :{0}", inst)
+    TestAppLogger.Warn("Part Number Check Python Script:  Invalid raw hex string")
+    isSuccess = False
+TestAppLogger.Info("Part Number Check Python Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#	Date: 04202022
+#	Version: 1.0
+#	Change: Initial Version
+############################################################
+
+
