9b2ad64c9f883c8d2ab1eab2b8c575a6ee5b262f
hjing-tpi <hjing@tpiusa.com>
2025-03-03 15:19:04 -0500

    Added TPass Scripts\ButtonPushStartTestCycle-Grupo_v2 - EJ IP.py - 2025-03-03T20:10:12.676Z

---
+++ TPass Scripts\ButtonPushStartTestCycle-Grupo_v2 - EJ IP.py
@@ -0,0 +1,64 @@
+﻿#Main Button Triggered Script
+#This Script can run any python logic and can also run a TPass Test Application
+#TPass Objects Passed In/Returned
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "2.0"
+production = False
+
+#########################################################################################################################################
+# Application Engineer:  Specific constant variables to maintain
+#
+testApplicationScriptFileName = "EJ_01.json"
+optionCodeFileName = "OptionsKM49.json"
+#
+#########################################################################################################################################
+
+TPassLogger.Debug("Main Button Script Begin")
+
+try:
+        TPassLogger.Debug("Main Button Script: Run a Test Application")
+        input = MainTPassScripting.GetInputKeyPad()
+        if input != "":
+            # Remove prefix/suffix characters
+            input = input.replace('#', '')
+            input = input.replace('$', '')
+            # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId,
+            #                bool useDefaultOptionCodesOnly = false, bool processTestResults = true, 
+            #                bool useCurrentProductIdentification = false, bool bypassOverrideTestAppScript = false)
+            if MainTPassScripting.RunTestCycle(testApplicationScriptFileName, optionCodeFileName, input, False, True, False, True):
+                isSuccess = True
+            else:
+                isSuccess = False
+                TPassLogger.Warn("Main Button Script:  Error Running Quick Test Cycle")
+        else:
+            isSuccess = True
+            TPassLogger.Info("GetInputKeyPad() No Job Entered by Operator")
+
+except Exception as inst:
+    TPassLogger.Warn("Main Button Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Main Button Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Main Button Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 11152024
+#   Version: 2.0
+#   ChangeBy: RMM
+#   Change: Remove prefix/suffix characters from scan input
+#   Date: 09182023
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
+
