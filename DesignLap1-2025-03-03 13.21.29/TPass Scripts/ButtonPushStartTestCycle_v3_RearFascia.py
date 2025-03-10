72493a36f035bbc2046fa2e44b518710dda0df4b
hjing-tpi <hjing@tpiusa.com>
2025-03-10 09:47:43 -0400

    Added DesignLap1-2025-03-03 13.21.29/TPass Scripts/ButtonPushStartTestCycle_v3_RearFascia.py

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/ButtonPushStartTestCycle_v3_RearFascia.py
@@ -0,0 +1,61 @@
+﻿#Main Button Triggered Script
+#This Script can run any python logic and can also run a TPass Test Application
+#TPass Objects Passed In/Returned
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "2.0"
+production = False
+
+#########################################################################################################################################
+# Application Engineer:  Specific constant variables to maintain
+#
+testApplicationScriptFileName = "L246_FasciaREAR_01.json"
+optionCodeFileName = "Options.json"
+#
+#########################################################################################################################################
+
+TPassLogger.Debug("Main Button Script Begin")
+
+try:
+        TPassLogger.Debug("Main Button Script: Run a Test Application")
+        input = MainTPassScripting.GetInputKeyPad()
+        # Get last 6 characters of passed in string.  Scan could be 9 digits
+        input = input[-6:]
+        if input != "":
+            # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true, bool useCurrentProductIdentification = false)
+            if MainTPassScripting.RunTestCycle("L246_FasciaREAR_01.json", "Options.json", input, False, True, False):
+                isSuccess = True
+            else:
+                isSuccess = False
+                TPassLogger.Warn("Main Button Script:  Error Running Quick Test Cycle")
+        else:
+            isSuccess = True
+            TPassLogger.Info("GetInputKeyPad() No Job Entered by Operator")
+
+except Exception as inst:
+    TPassLogger.Warn("Main Button Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Main Button Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Main Button Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 10212021
+#   Version: 2.0
+#   ChangeBy: RMM
+#   Change: Updated to standards
+#   Date: 01172021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
+
