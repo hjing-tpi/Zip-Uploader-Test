7b1e8645c8766f5d34aa171a702ef6ac999c4251
hjing-tpi <hjing@tpiusa.com>
2025-03-10 08:56:27 -0400

    Added DesignLap1-2025-03-03 13.21.29/Test App Scripts/PartNumberCheck-Ascii-NoRevision.py

---
+++ DesignLap1-2025-03-03 13.21.29/Test App Scripts/PartNumberCheck-Ascii-NoRevision.py
@@ -0,0 +1,61 @@
+﻿#Part Number Check.  Parse raw data ASCII and No Revision
+#
+#TPass Objects Passed In/Returned
+#   in - string "broadcastedPartNumber"
+#   in - string "rawCanDataResponse"
+#   in - Function "TestAppLogger"
+#   out - string "processedPartNumber
+#   out - string "rawCanDataPartNumberHex"
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+
+#System.Diagnostics.Debugger.Break();
+
+#rawCanDataResponse = "100D62F13236383137313139334142"
+#broadcastedPartNumber = '12345678A'
+#print('Raw can data response = ', rawCanDataResponse)
+
+import clr
+version = "1.0"
+production = False
+
+# string offset starts with 0
+requiredRawDataLength = 30
+partNumberHexByteOffset = 10
+isSuccess = True
+TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data Response = {0}", rawCanDataResponse)
+TestAppLogger.Info("Part Number Check Python Script:  Broadcasted Part Number = {0}", broadcastedPartNumber)
+if len(rawCanDataResponse) != requiredRawDataLength:
+	TestAppLogger.Warn("Part Number Check Python Script:  Raw Can Data Response length must be equal to {0}.  Length = {1}", requiredRawDataLength, len(rawCanDataResponse))
+	isSuccess = False
+else:
+	rawCanDataWithoutRev = rawCanDataResponse[partNumberHexByteOffset:-2]
+	TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data without Header and Revision = {0}", rawCanDataWithoutRev)
+	rawCanDataPartNumberHex = rawCanDataWithoutRev
+
+	broadcastedPartNumberWithoutRev = broadcastedPartNumber[:-1]
+	TestAppLogger.Info("Part Number Check Python Script:  Broadcasted Part Number without Revision = {0}", broadcastedPartNumberWithoutRev)
+	try:
+		processedPartNumber = bytes.fromhex(rawCanDataWithoutRev)
+		TestAppLogger.Info("Part Number Check Python Script:  Processed Part Number = {0}", processedPartNumber)
+		if broadcastedPartNumberWithoutRev != processedPartNumber:
+			isSuccess = False
+	except Exception as inst:
+		TestAppLogger.Warn("Part Number Check Python Script:  Exception Occurred :{0}", inst)
+		TestAppLogger.Warn("Part Number Check Python Script:  Invalid raw hex string")
+		isSuccess = False
+TestAppLogger.Info("Part Number Check Python Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#	Date: 01012019
+#	Version: 1.0
+#	Change: Initial Version
+############################################################
+
+
+
