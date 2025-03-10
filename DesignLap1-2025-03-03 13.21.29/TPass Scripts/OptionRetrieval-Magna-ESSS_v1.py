5ac7a75972a17408e7f8333fb77ad44b85e586a5
hjing-tpi <hjing@tpiusa.com>
2025-03-10 09:49:07 -0400

    Added DesignLap1-2025-03-03 13.21.29/TPass Scripts/OptionRetrieval-Magna-ESSS_v1.py

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/OptionRetrieval-Magna-ESSS_v1.py
@@ -0,0 +1,59 @@
+﻿#Retrieve Build Data for Product Id
+#This Script is expected to set the out parameters below
+#
+#TPass Objects Passed In/Returned
+#   in  - string "RunTestCycleId" - This is the data that was passed from the Start Test Cycle script via the RunTestCycle method.
+#   in  - object "MainTPassScripting" - Exposed methods and properties for the scripts to use
+#   in  - method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   in  - method "TestAppLogger" - This is the logging method to log to the TPass Test Application Detail log file
+#   in  - object "SystemConfigurationValue" - This is the object to get values set in the System Configuration file
+#   out - string "productBuildData" - Build data retrieved to be processed by the Option Parsing script
+#   out - bool "isSuccess" - Set to True if build data retrieval was successful.  Otherwise False
+#   out - bool "production"
+#   out - string "version"
+#
+
+from System.IO import File
+from System import DateTime
+
+version = "1.0"
+production = False
+productBuildData = ""
+isSuccess = False
+
+
+#########################################################################################################################################
+# Application Engineer:  Specific constant variables to maintain
+#
+
+#
+#########################################################################################################################################
+
+
+# Main Logic
+try:
+
+    TPassLogger.Debug("Option Retrieval Script:  Product Id = {0}", RunTestCycleId)
+    # InterfaceUiLogger(string mesSystem, string detail, bool isError = false, bool alarm = false)
+    MainTPassScripting.InterfaceUiLogger("Magna-ESSS", "Trigger Type = " + str(MainTPassScripting.StartTestCycleTriggerType), False, False)
+    MainTPassScripting.InterfaceUiLogger("Magna-ESSS", "Data = " + RunTestCycleId, False, False)
+    #MainTPassScripting.InterfaceUiLogger("Magna-ESSS", "Build data complete no need to interface with Magna-ESSS", False, False)
+    #TPassLogger.Debug("Option Retrieval Script - Build data complete no need to interface with Magna-ESSS")
+    productBuildData = RunTestCycleId
+    isSuccess = True
+
+
+except Exception as inst:
+    MainTPassScripting.InterfaceUiLogger("Magna-ESSS", "Get Build Data Failed: " + str(inst), True, True)
+    TPassLogger.Warn("Option Retrieval Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Option Retrieval Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Option Retrieval Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 04172023
+#   Version: 1.0
+#   Change: Initial Version
+############################################################
