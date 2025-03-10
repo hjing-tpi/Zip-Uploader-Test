9878b58e661bccb24c249896fff315c29890f761
hjing-tpi <hjing@tpiusa.com>
2025-03-10 08:57:59 -0400

    Added DesignLap1-2025-03-03 13.21.29/TPass Scripts/ButtonPushStartTestCycle_Magna-ESSS_v3.py

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/ButtonPushStartTestCycle_Magna-ESSS_v3.py
@@ -0,0 +1,74 @@
+﻿#Main Button Triggered Script
+#This Script can run any python logic and can also run a TPass Test Application
+#TPass Objects Passed In/Returned
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "3.0"
+production = False
+
+#########################################################################################################################################
+# Application Engineer:  Specific constant variables to maintain
+#
+testApplicationScriptFileName = "KtbWirelessBuckleTest.json"
+optionCodeFileName = "KtbWirelessBuckleOptions.json"
+password = "GEORGE"
+#
+#########################################################################################################################################
+
+TPassLogger.Debug("Main Button Script Begin")
+
+try:
+        TPassLogger.Debug("Main Button Script: Run a Test Application")
+        #Get Password
+        pwInput = MainTPassScripting.GetInputKeyBoard("Password", True)
+        if pwInput != "":
+            if pwInput.ToUpper() != password:
+                # InterfaceUiLogger(string mesSystem, string detail, bool isError = false, bool alarm = false)
+                MainTPassScripting.InterfaceUiLogger("Magna-ESSS", "Invalid Password", True, True)
+            else:
+                input = MainTPassScripting.GetInputKeyPad("Recipe")
+                if input != "":
+                    # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true, bool useCurrentProductIdentification = false, bool bypassOverrideTestAppScript = false)
+                    if MainTPassScripting.RunTestCycle(testApplicationScriptFileName, optionCodeFileName, input, False, True, False, False):
+                        isSuccess = True
+                    else:
+                        isSuccess = False
+                        TPassLogger.Warn("Main Button Script:  Error Running Test Cycle")
+                else:
+                    isSuccess = True
+                    TPassLogger.Info("GetInputKeyPad() No Recipe Entered by Operator")
+        else:
+            isSuccess = True
+            TPassLogger.Info("GetInputKeyBoard() No Password Entered by Operator")
+
+except Exception as inst:
+    TPassLogger.Warn("Main Button Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Main Button Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Main Button Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 06192023
+#   Version: 3.0
+#   ChangeBy: RMM
+#   Change: Added password prompt
+#   Date: 06062023
+#   Version: 2.0
+#   ChangeBy: RMM
+#   Change: Updated to standards
+#   Date: 04172023
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
+
