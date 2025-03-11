29c71410a2511637778ec8d51c356909c950e269
hjing-tpi <hjing@tpiusa.com>
2025-03-11 15:12:32 -0400

    Added DesignLap1-2025-03-03 13.21.29/Test App Scripts/PartNumberCheck-OPSOFT-GM-GlobalB_v1.py

---
+++ DesignLap1-2025-03-03 13.21.29/Test App Scripts/PartNumberCheck-OPSOFT-GM-GlobalB_v1.py
@@ -0,0 +1,50 @@
+﻿#Part Number Check.  Parse raw data HEX 8 in Long Part Number
+#
+#TPass Objects Passed In/Returned
+#   in - string "broadcastedPartNumber"
+#   in - string "rawCanDataResponse"
+#   in - Function "TestAppLogger"
+#   out - string "processedPartNumber"
+#   out - string "rawCanDataPartNumberHex"
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+
+#System.Diagnostics.Debugger.Break();
+
+#rawCanDataResponse = "065ACB0000CE5FBAAA"
+#broadcastedPartNumber = '13524922'
+
+import clr
+version = "1.0"
+production = False
+
+# string offset starts with 0
+partNumberHexByteOffset = 14
+isSuccess = True
+try:
+    TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data Response = {0}", rawCanDataResponse)
+    TestAppLogger.Info("Part Number Check Python Script:  Broadcasted Part Number = {0}", broadcastedPartNumber)
+    rawCanDataPartNumberHex = rawCanDataResponse[partNumberHexByteOffset:partNumberHexByteOffset+8]
+    TestAppLogger.Info("Part Number Check Python Script:  Raw Can Data Part Number Hex = {0}", rawCanDataPartNumberHex)
+    processedPartNumber = long(rawCanDataPartNumberHex, 16)
+    TestAppLogger.Info("Part Number Check Python Script:  Processed Part Number = {0}", processedPartNumber)
+    if processedPartNumber != long(broadcastedPartNumber):
+        isSuccess = False
+except Exception as inst:
+    TestAppLogger.Warn("Part Number Check Python Script:  Exception Occurred :{0}", inst)
+    TestAppLogger.Warn("Part Number Check Python Script:  Invalid raw hex string")
+    isSuccess = False
+TestAppLogger.Info("Part Number Check Python Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 04262021
+#   Version: 1.0
+#   Change: Initial Version
+############################################################
+
+
