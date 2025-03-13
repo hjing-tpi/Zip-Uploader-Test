47898aea47e50702c5bd323826114b34e66ddd82
hjing-tpi <hjing@tpiusa.com>
2025-03-13 14:12:41 -0400

    Batch upload: DesignLap1-2025-03-03 13.21.29.zip

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/runallthetime.py
@@ -0,0 +1,58 @@
+﻿# Process Product Input Data sent to TPass, either by Scanner, File Drop, Serial etc.
+# This Script is expected to call RunTestCycle() passing in the appropriate parameters and setting the out parameters below
+#
+# TPass Objects Passed In and expected Returned
+#   in  - string "StartCycleInputData" - This is the data that triggered this script by either a bar code scan, file drop, etc...
+#   in  - object "MainTPassScripting" - Exposed methods and properties for the scripts to use
+#   in  - method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   in  - method "TestAppLogger" - This is the logging method to log to the TPass Test Application log file
+#   in  - method "GetConfigurationValue" - This is the method to get values set in the System Configuration file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+import clr
+version = "1.0"
+production = False
+
+TPassLogger.Debug("Start Test Cycle Script:  Product Input Data = {0}", StartCycleInputData)
+
+try:
+
+    TPassLogger.Debug("Start Test Cycle Script: Request and processing build data, run a Test Application and process the results")
+
+    # If required to parse the StartCycleInputData differently based on what input type triggered the script to run (Scan, Manual Entry, File Drop...)
+    if str(MainTPassScripting.StartTestCycleTriggerType) == str(MainTPassScripting.TriggerType.Scan):
+        TPassLogger.Debug("StartTestCycle - Trigger Type is Scan")
+    elif str(MainTPassScripting.StartTestCycleTriggerType) == str(MainTPassScripting.TriggerType.ManualEntry):
+        TPassLogger.Debug("StartTestCycle - Trigger Type is ManualEntry")
+    elif str(MainTPassScripting.StartTestCycleTriggerType) == str(MainTPassScripting.TriggerType.Udp):
+        TPassLogger.Debug("StartTestCycle - Trigger Type is Udp")
+
+    # If required, parse the StartCycleInputData and pass in to the RunTestCycle() method.  This data will be used by the Request Script for build data retrieval
+    runTestCycleId = StartCycleInputData
+
+    # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true)
+    if MainTPassScripting.RunTestCycle("IO Board Checkout (32 Ch).json", "32 Ch Options.json", runTestCycleId, False, True):
+#    if MainTPassScripting.RunTestCycle("GM Tester v01.json", "GM Options v01.json", runTestCycleId, False, True):
+        isSuccess = True
+    else:
+        isSuccess = False
+        TPassLogger.Warn("Start Test Cycle Script:  Error Running Test Cycle")
+
+except Exception as inst:
+    TPassLogger.Warn("Start Test Cycle Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Start Test Cycle Script:  Processing Failed.")
+    isSuccess = False
+
+TPassLogger.Info("Start Test Cycle Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 04162021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+###########################
