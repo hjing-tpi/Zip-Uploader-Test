c95a1c701e0e482fb21046369c635974edb9c9ca
hjing-tpi <hjing@tpiusa.com>
2025-03-13 08:57:13 -0400

    Batch upload: DesignLap1-2025-03-03 13.21.29.zip

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/IAC Pass.py
@@ -0,0 +1,55 @@
+﻿#Main Button Triggered Script
+#This Script can run any python logic and can also run a TPass Test Application
+#TPass Objects Passed In/Returned
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "1.9"
+production = False
+
+#########################################################################################################################################
+# Application Engineer:  Specific constant variables to maintain
+#
+testApplicationScriptFileName = "SwitchTestDelay.json"
+optionCodeFileName = "OptionCodes-Tesla.json"
+#
+#########################################################################################################################################
+
+TPassLogger.Debug("Main Button Script Begin")
+
+try:
+        TPassLogger.Debug("Main Button Script: Run a Test Application")
+        input = MainTPassScripting.GetInputKeyPad()
+        if input != "":
+            input = "Unknown;" + input
+            # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true, bool useCurrentProductIdentification = false, bool bypassOverrideTestAppScript = false)
+            if MainTPassScripting.RunTestCycle(testApplicationScriptFileName, optionCodeFileName, input, False, True, False, False):
+                isSuccess = True
+            else:
+                isSuccess = False
+                TPassLogger.Warn("Main Button Script:  Error Running Quick Test Cycle")
+        else:
+            isSuccess = True
+            TPassLogger.Info("GetInputKeyPad() No Recipe Entered by Operator")
+        
+except Exception as inst:
+    TPassLogger.Warn("Main Button Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Main Button Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Main Button Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 01172021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/OverrideTestApplicationDakkota_v1.py
@@ -0,0 +1,57 @@
+﻿#Override Test Application - Parse the build data passed in from the request script and set the product attribute variables
+#This Script is expected to set the out parameter below
+#
+#TPass Objects Passed In/Returned
+#   in - string "currentTestApplicationScriptFileName"
+#   in - string "primaryId"
+#   in - string "secondaryId"
+#   in - string "tertiaryId"
+#   in - string "quaternaryId"
+#   in - string "quinaryId"
+#   in - string "buildData"
+#   in - List<string> "activeOptionCodes" 'All options are upper case
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - string "newTestApplicationScriptFileName"
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+from System.Text.RegularExpressions import Regex
+
+production = False
+version = "1.0"
+
+
+TPassLogger.Debug("Override Test Application Script:  Product ID = {0}", primaryId)
+#MainTPassScripting.InterfaceUiLogger("MES", "Override Test Application", True, True)
+
+try:
+    newTestApplicationScriptFileName = currentTestApplicationScriptFileName
+    # Set new test application script file name
+    if activeOptionCodes.Contains("68504658AA"):
+        newTestApplicationScriptFileName = "WD_IP_01.json"
+    elif activeOptionCodes.Contains("68376607AA"):
+        newTestApplicationScriptFileName = "WL_IP_01.json"
+
+    TPassLogger.Debug("Override Test Application Script:  New Test Application Script File Name = {0}", newTestApplicationScriptFileName )
+    isSuccess = True
+
+except Exception as inst:
+    TPassLogger.Warn("Override Test Application Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Override Test Application Script:  Processing Failed.")
+    isSuccess = False
+    
+TPassLogger.Info("Override Test Application Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 11042021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
+
