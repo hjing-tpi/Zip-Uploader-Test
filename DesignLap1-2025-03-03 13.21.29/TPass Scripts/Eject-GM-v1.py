e3624076cf641dfbddc3db00c01145689fba463e
hjing-tpi <hjing@tpiusa.com>
2025-03-14 08:25:21 -0400

    Batch upload: DesignLap1-2025-03-03 13.21.29.zip

---
+++ DesignLap1-2025-03-03 13.21.29/TPass Scripts/Eject-GM-v1.py
@@ -0,0 +1,42 @@
+﻿#Main Button Triggered Script
+#This Script can run any python logic and can also run a TPass Test Application
+#TPass Objects Passed In/Returned
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "1.0"
+production = False
+
+TPassLogger.Debug("Main Button Script Begin")
+
+try:
+    TPassLogger.Info("Main Button Script: Run a Quick Test Application with default options only and don't process results")
+    # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true, bool useCurrentProductIdentification = false)
+    if MainTPassScripting.RunTestCycle("Eject All.json", "Options.json", "", True, False, False):
+        isSuccess = True
+    else:
+        isSuccess = False
+        TPassLogger.Warn("Main Button Script:  Error Running Quick Test Cycle")
+
+except Exception as inst:
+    TPassLogger.Warn("Main Button Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Main Button Script:  Processing Failed.")
+    isSuccess = False
+TPassLogger.Info("Main Button Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 01172021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+############################################################
+
