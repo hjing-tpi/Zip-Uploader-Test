f3e8481628d272a2a08c1a08c2a6945cac987617
hjing-tpi <hjing@tpiusa.com>
2025-03-24 11:08:26 -0400

    Upload by John: This is word
    Files: about 50MB file.zip

---
+++ DesignLap1-2025-03-03 13.21.29/DesignLap1-2025-03-03 13.21.29/TPass Scripts/Archive/StartTestCycle-TPI-PAPR-Main-v1.py
@@ -0,0 +1,49 @@
+﻿# Process Product Input Data sent to TPass, either by Keyboard, Scanner, File Drop, Serial etc.
+# This Script is expected to call RunTestCycle() passing in the appropriate parameters
+#
+# TPass Objects Passed In and expected Returned
+#   in  - string StartCycleInputData - This is the data that triggered this script by either manual entry, a bar code scan, file drop, etc...
+#   in  - object MainTPassScripting - Exposed all methods and properties for the scripts to use
+#   in  - Method "TPassLogger" - This is the logging method to log to the main TPass log file
+#   out - bool "isSuccess"
+#   out - bool "production"
+#   out - string "version"
+#
+
+#System.Diagnostics.Debugger.Break();
+
+import clr
+version = "1.0"
+production = False
+
+TPassLogger.Debug("Start Test Cycle Script:  Product Input Data = {0}", StartCycleInputData)
+
+try:
+
+    TPassLogger.Debug("Start Test Cycle Script: Request and processing build data, run a Test Application and process the results")
+
+    # If required parse the StartCycleInputData and pass in to the RunTestCycle() method.  This data will be used by the Request Script Data for build data retrieval
+    runTestCycleId = StartCycleInputData
+
+    # RunTestCycle(string testApplicationScriptFileName, string optionCodeFileName, string runTestCycleId, bool useDefaultOptionCodesOnly = false, bool processTestResults = true)
+    if MainTPassScripting.RunTestCycle("PAPR Main Tester 01.json", "SC-PAPR-Battery_01.json", runTestCycleId, False, True):
+        isSuccess = True
+    else:
+        isSuccess = False
+        TPassLogger.Warn("Start Test Cycle Script:  Error Running Test Cycle")
+
+except Exception as inst:
+    TPassLogger.Warn("Start Test Cycle Script:  Exception Occurred :{0}", inst)
+    TPassLogger.Warn("Start Test Cycle Script:  Processing Failed.")
+    isSuccess = False
+
+TPassLogger.Info("Start Test Cycle Script:  Is Success = {0}", isSuccess)
+
+############################################################
+# Change History
+############################################################
+#   Date: 01172021
+#   Version: 1.0
+#   ChangeBy: RMM
+#   Change: Initial Version
+###########################
